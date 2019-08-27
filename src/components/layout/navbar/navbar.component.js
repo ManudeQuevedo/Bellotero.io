107a1f9cdbf335d92b9558eca9abded0bdd24ae6
Manu de Quevedo <hola@manudequevedo.com>
2019-08-27 17:56:26 -0500

    First commit

---
+++ src/components/layout/navbar/navbar.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.styles.css'
+
+
+const Navbar = () => {
+   return (
+      <React.Fragment>
+         <nav className="navbar-grid">
+            <div className="navbar-wrapper">
+               <div className="navbar-logo" />
+            </div>
+         </nav>
+      </React.Fragment>
+   )
+}
+
+export default Navbar
